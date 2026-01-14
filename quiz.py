0d2862ba140206be43a23478285012d145a07dcd
PirunthaM <mpiruntha57@gmail.com>
2026-01-14 14:37:01 +0000

    gitignore added

--- quiz.py
+++ quiz.py
@@ -1,5 +1,4 @@
 score = 0
-current_questions = 0
 total_questions = 5
 
 questions = [
@@ -11,45 +10,14 @@ questions = [
 ]
 
 options = [
-    ["6", "15", "8", "10"],
-    ["8", "6", "7", "5"],
-    ["42", "36", "48", "56"],
-    ["5", "4", "6", "8"],
-    ["18", "19", "20", "21"]
+    ["1) 6", "2) 15", "3) 8", "4) 10"],
+    ["1) 8", "2) 6", "3) 7", "4) 5"],
+    ["1) 42", "2) 36", "3) 48", "4) 56"],
+    ["1) 5", "2) 4", "3) 6", "4) 8"],
+    ["1) 18", "2) 19", "3) 20", "4) 21"]
 ]
 
-correct_answers = [3, 1, 1, 1, 2]
+answers = [3, 1, 1, 1, 2]
 
 print("Hello, the quiz will now begin...")
-
-while current_questions<total_questions:
-    print(questions[current_questions], current_questions + 1)
-
-    current_options = options[current_questions]
-
-    print(current_options)
-
-    guess = int(input("Enter your answer between 1-4..."))
-
-    if guess != "2" and guess != "2" and guess != "3" and guess != "4":
-        print("Thank you for your answer")
-    else:
-        print("That is an incorrect option, please choose between 1-4")
-
-    correct_answer = correct_answers[current_questions]
-
-    if guess == correct_answer:
-        score = score + 1
-        print("Well done, that is correct")
-    else:
-        print("incorrect")
-
-    current_question = current_questions + 1
-
-print("Thank you for participating. Your score and percentage will be displayed below...")
-print ("Your final score is ", score)
-
-percentage = (score / total_questions) * 100
-print("Your final percentage is ", percentage)
-
 
