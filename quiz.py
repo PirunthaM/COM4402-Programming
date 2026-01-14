0cbe7480459705dcea00ed1a70a28029edc75a44
PirunthaM <mpiruntha57@gmail.com>
2026-01-14 12:36:38 +0000

    gitignore added

---
+++ quiz.py
@@ -0,0 +1,23 @@
+score = 0
+current_questions = 0
+total_questions = 5
+
+questions = [
+    "What is 5 + 3?",
+    "What is 12 - 4?",
+    "What is 7 x 6?",
+    "What is 25 % 5?",
+    "What is 9 + 10?",
+]
+
+options = [
+    ["6", "15", "8", "10"],
+    ["8", "6", "7", "5"],
+    ["42", "36", "48", "56"],
+    ["5", "4", "6", "8"],
+    ["18", "19", "20", "21"]
+]
+
+correct_answers = [3, 1, 1, 1, 2]
+
+print("Hello, the quiz will now begin...")
